0bdf831307aebb1d2dc657c4f60639bd663d3854
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-17 14:39:29 +0530

    adding navigation bar item

--- foodires/includes/header.php
+++ foodires/includes/header.php
@@ -6,7 +6,23 @@
 		
 	</div>
 
-    
+    <div class="main_nav">
+		
+		<h2>Unique Restaurant</h2>
+		
+		<ul class="default_links">
+			
+			<li><a href="index.php">Home</a></li>
+			<li><a href="menu.php">Menu</a></li>
+			<li><a href="reservation.php">Reservation</a></li>
+			<li><a href="gallery.php">Gallery</a></li>
+			<li><a href="basket.php">Order</a></li>
+			
+		</ul>
+		
+		<p class="clear"></p>
+		
+	</div>
 	
 	
 </header>
