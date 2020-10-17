1ca60c44f25e8b90a4ee8bf05c78f60a67096f9f
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-17 17:10:58 +0530

    class parallax item to fetch details of items like food name description and price

--- foodires/index.php
+++ foodires/index.php
@@ -9,7 +9,17 @@
 			$result .= "<div class='parallax_item'>
 				
                             <a href='detail.php?fid=".$row['id']."'><img src='image/FoodPics/".$row['id'].".jpg' width='80px' height='80px' />
-                             
+                            <div class='detail'>
+								
+                            <h4>".$row['food_name']."</h4>
+                            <p class='desc'>".substr($row['food_description'], 0, 33)."...</p>
+                            <p class='price'>#".$row['food_price']."</p>
+                            
+                        </div>
+                        <p class='clear'></p>
+                        </a>
+                        
+                    </div>";
     }
 }
 
