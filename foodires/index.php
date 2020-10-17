1c1e1b8e93b8ab3cf8557b7e973f03566e67ee8c
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-18 00:02:01 +0530

    title foodires

--- foodires/index.php
+++ foodires/index.php
@@ -29,7 +29,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content=""/>
-    <title>Document</title>
+    <title>foodires</title>
 </head>
 <body>
     
