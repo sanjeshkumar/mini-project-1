39e92d397c0d3cd5f41561d9233bdd76d042d862
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-18 00:10:47 +0530

    including header

--- foodires/index.php
+++ foodires/index.php
@@ -29,9 +29,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content=""/>
-    <title>foodires</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <script src="js/myscript.js"></script>
 </head>
 <body>
-    
+<?php require "includes/header.php"; ?>
 </body>
 </html>
