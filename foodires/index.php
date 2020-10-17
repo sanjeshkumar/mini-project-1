2f602ce0fa548349b5d9a66733ece3eff959e855
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-17 17:07:33 +0530

    to fetch images of food items

--- foodires/index.php
+++ foodires/index.php
@@ -4,6 +4,13 @@
     $get_recent = $db->query("SELECT * FROM food LIMIT 9");
     $result = "";
     if($get_recent->num_rows) {
+        while($row = $get_recent->fetch_assoc()) {
+			
+			$result .= "<div class='parallax_item'>
+				
+                            <a href='detail.php?fid=".$row['id']."'><img src='image/FoodPics/".$row['id'].".jpg' width='80px' height='80px' />
+                             
     }
+}
 
     ?>
