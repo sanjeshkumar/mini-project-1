c2fea19bed9d5c948b438e59cb7a50810e14e73e
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-17 17:16:28 +0530

    html 5 description

--- foodires/index.php
+++ foodires/index.php
@@ -22,5 +22,16 @@
                     </div>";
     }
 }
-
-    ?>
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""/>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
