0fbd7d9662487ae682d9425d8406111cf1d6f788
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-18 08:49:34 +0530

    cart quantity

--- foodires/basket.php
+++ foodires/basket.php
@@ -13,6 +13,12 @@
 		$qty = isset($_GET['qty']) ? (int)$_GET['qty'] : 1;
 		$wasFound = false;
         $i = 0;
-        
+        if (!isset($_SESSION["cart_array"]) || count($_SESSION["cart_array"]) < 1) { 
+			
+			$_SESSION["cart_array"] = array(0 => array("item_id" => $fid, "quantity" => $qty));
+		}
+        else {
+			
+			$qty = isset($_GET['qty']) ? (int)$_GET['qty'] : 1;
     }
     ?>
