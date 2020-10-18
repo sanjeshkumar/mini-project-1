690dc70182d1454bd5e46b4d9fe0edafaac0e274
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-18 08:54:49 +0530

    while loop having array splice to adjust quantity

--- foodires/basket.php
+++ foodires/basket.php
@@ -22,5 +22,16 @@
             $qty = isset($_GET['qty']) ? (int)$_GET['qty'] : 1;
             foreach ($_SESSION["cart_array"] as $each_item) { 
                 $i++;
+                while (list($key, $value) = each($each_item)) {
+                    if ($key == "item_id" && $value == $fid) {
+                        // That item is in cart already so let's adjust its quantity using array_splice()
+                        array_splice($_SESSION["cart_array"], $i-1, 1, array(array("item_id" => $fid, "quantity" => $each_item['quantity'] + $qty)));
+                        $wasFound = true;
+                    } 
+                }
     }
+    if ($wasFound == false) {
+        array_push($_SESSION["cart_array"], array("item_id" => $fid, "quantity" => $qty));
+    }
+}
     ?>
