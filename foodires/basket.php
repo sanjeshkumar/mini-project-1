39f0edf5c589b935298d277adc022c2b28d21f6e
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-18 12:48:11 +0530

    fetching the item by foreach loop

--- foodires/basket.php
+++ foodires/basket.php
@@ -86,4 +86,9 @@ if (!isset($_SESSION["cart_array"]) || count($_SESSION["cart_array"]) < 1) {
                     
     
     $i = 0;
+
+    foreach ($_SESSION["cart_array"] as $each_item) { 
+        $item_id = $each_item['item_id'];
+        $sql = $db->query("SELECT * FROM food WHERE id='$item_id' LIMIT 1");
+    }
 ?>
