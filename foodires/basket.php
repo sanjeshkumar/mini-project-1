607eed84f2d6aa04f32362394bb3cd8b38e2d860
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-18 08:44:52 +0530

    isset expression return true if nthere is submit

--- foodires/basket.php
+++ foodires/basket.php
@@ -5,3 +5,14 @@
 	require "admin/includes/db.php";
 	
 ?>
+
+<?php 
+
+	if (isset($_GET['fid']) && isset($_GET['qty'])) {
+		$fid = $_GET['fid'];
+		$qty = isset($_GET['qty']) ? (int)$_GET['qty'] : 1;
+		$wasFound = false;
+        $i = 0;
+        
+    }
+    ?>
