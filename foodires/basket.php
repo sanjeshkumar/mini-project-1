46485c207b754ab4c3e8b02f4a9352a66c97e867
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-18 09:01:30 +0530

    if user wants to remove an item from cart

--- foodires/basket.php
+++ foodires/basket.php
@@ -37,3 +37,26 @@
 		exit();
 }
     ?>
+
+<?php
+ 
+ if (isset($_GET['cmd']) && $_GET['cmd'] == "emptycart") {
+     unset($_SESSION["cart_array"]); //empty the foody cart
+ }
+ 
+?>
+
+<?php 
+
+	if (isset($_POST['index_to_remove']) && $_POST['index_to_remove'] != "") {
+		// Access the array and run code to remove that array index
+		$key_to_remove = $_POST['index_to_remove'];
+		if (count($_SESSION["cart_array"]) <= 1) {
+			unset($_SESSION["cart_array"]);
+		} else {
+			unset($_SESSION["cart_array"]["$key_to_remove"]);
+			sort($_SESSION["cart_array"]);
+		}
+	}
+	
+?>
